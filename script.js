5709d2059040b15c5076cbd803d2cded4486bacf
mathcodes <jonpchristie@gmail.com>
2022-11-16 05:58:42 -0500

    color and icon picker in form working 👍

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const cls = ['fa-solid', 'fa', 'fa-coffee', 'fa-user']
+const cls = ['fa-solid',  'fa-coffee', 'fa-user']
 function showFormData(oForm) {
 
-  const f1 = document.getElementById("firstReturn").textContent = oForm.elements[0].value;
+  const icon1 = document.getElementById("firstReturn").textContent = oForm.elements[0].value;
   document.getElementById("firstIcon").classList.remove(...cls);
-  document.getElementById("firstIcon").classList.add(f1)
-// fa-solid fa-user
-
+  let newClass1 = "fa-solid " + icon1 + "";
+  console.log({newClass1})
+  const color1 = document.getElementById("firstIcon").classList.add(icon1)
+  
+  // const color1 = document.getElementById("favcolor").textContent = oForm.elements[0].value;
+  // document.getElementById("secondIcon").value = color1;
+  // document.getElementById("favcolor").setAttribute("value",color1);
+  let colorChoice = oForm.elements[1].value;
+  console.log({colorChoice})
+  // document.getElementById("favcolor").setAttribute("value","");
+  document.getElementById('aSecondIcon').style.background = colorChoice;
   return(f1);
 }
 
